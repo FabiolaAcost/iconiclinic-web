7f13a642c7a78b09defcd3520dccaaad1e8bd003
FabiolaAcost <fabiestefania307@gmail.com>
2025-07-19 19:58:47 -0400

    feat():adding cors enabled

--- proyectoBackend/server.js
+++ proyectoBackend/server.js
@@ -2,16 +2,25 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const PORT = 3000;
+
 const authRoutes = require("./routes/auth.routes");
 const profesionalRoutes = require("./routes/profesional.routes");
 const pacienteRoutes = require("./routes/paciente.routes");
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "https://iconiclinic.netlify.app",
+    "http://localhost:5500",          
+    "http://127.0.0.1:5500"          
+  ]
+}));
+
 app.use(express.json());
+
 app.use("/api", authRoutes);
 app.use("/api/profesional", profesionalRoutes);
 app.use("/api/paciente", pacienteRoutes);
 
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`✅ Servidor corriendo en http://localhost:${PORT}`);
 });

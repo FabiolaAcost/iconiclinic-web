e13e62bce3cfe865a25b1ffee3f2675cfd53bdc6
FabiolaAcost <fabiestefania307@gmail.com>
2025-07-19 22:01:58 -0400

    fix():fix controller paciente

--- proyectoBackend/routes/paciente.routes.js
+++ proyectoBackend/routes/paciente.routes.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 const { obtenerRutinaPaciente, obtenerHistorialTratamientos, obtenerUltimaRecomendacion } = require("../controllers/paciente.controller");
 
-router.get("/rutina/:id_paciente", obtenerRutinaPaciente);
-router.get("/historial/:id_paciente", obtenerHistorialTratamientos);
-router.get("/recomendacion/:id_paciente", obtenerUltimaRecomendacion);
+router.get("/rutina/:id_usuario", obtenerRutinaPaciente);
+router.get("/historial/:id_usuario", obtenerHistorialTratamientos);
+router.get("/recomendacion/:id_usuario", obtenerUltimaRecomendacion);
 
 module.exports = router;
